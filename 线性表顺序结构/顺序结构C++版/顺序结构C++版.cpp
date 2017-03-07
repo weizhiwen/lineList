2015101c1d95b297f0d473f2b149faf98df4430a
魏志文 <763107756@qq.com>
2017-03-07 09:34:16 +0800

    C++的实现

--- 线性表顺序结构/顺序结构C++版/顺序结构C++版.cpp
+++ 线性表顺序结构/顺序结构C++版/顺序结构C++版.cpp
@@ -2,10 +2,24 @@
 //
 
 #include "stdafx.h"
-
+#include <iostream>
+#include "SqList.h"
+using namespace std;
 
 int main()
 {
+	SqList L(5);
+	//测试插入函数
+	L.insertList(1, 1);
+	L.insertList(2, 2);
+	L.insertList(3, 3);
+	L.insertList(4, 4);
+	L.insertList(5, 5);
+	L.insertList(6, 6);
+	L.printList();
+	cout << "元素个数为" << L.listLength() << endl;
+	cout << "线性表长度为" << L.listSize() << endl;
+	system("pause");
     return 0;
 }
 
